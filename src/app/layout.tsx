469ab6ec93f6e655beac0975bb05ce818aa68e9d
Izaias Lima <izaiaslima356@gmail.com>
2025-03-09 23:10:15 -0300

    build: set up next 15 shadcn app

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import { Metadata } from "next";
+import { GeistSans } from "geist/font/sans";
+import "./globals.css";
+
+export const metadata: Metadata = {
+	title: "PDV",
+	description: "O melhor PDV do Brasil",
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html
+			lang="pt-br"
+			className={GeistSans.className}
+			suppressHydrationWarning={true}
+		>
+			<body className="antialised">{children}</body>
+		</html>
+	);
+}
---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,172 @@
+"use client";
+import * as React from "react";
+import {
+	AudioWaveform,
+	Banknote,
+	BookOpen,
+	Bot,
+	Command,
+	Database,
+	GalleryVerticalEnd,
+	LayoutDashboard,
+	MonitorSmartphone,
+	Receipt,
+	Rocket,
+	Settings,
+	Settings2,
+	ShieldCheck,
+	SquareTerminal,
+} from "lucide-react";
+import { NavMain } from "@/components/nav-main";
+import { NavUser } from "@/components/nav-user";
+import { TeamSwitcher } from "@/components/team-switcher";
+import {
+	Sidebar,
+	SidebarContent,
+	SidebarFooter,
+	SidebarHeader,
+	SidebarRail,
+} from "@/components/ui/sidebar";
+
+const data = {
+	user: {
+		name: "Izaías Lima",
+		email: "izaiaslima356@gmail.com",
+		avatar: "Izaías Lima",
+	},
+	teams: [
+		{
+			name: "São Cristóvão",
+			logo: GalleryVerticalEnd,
+			plan: "Aberto",
+		},
+		{
+			name: "Doom",
+			logo: AudioWaveform,
+			plan: "Fechado",
+		},
+		{
+			name: "Cocóbis",
+			logo: Command,
+			plan: "Fechado",
+		},
+	],
+	navMain: [
+		{
+			title: "Dashboard",
+			url: "/",
+			icon: LayoutDashboard,
+			isActive: false,
+			isPage: true,
+		},
+		{
+			title: "Vendas",
+			url: "/vendas",
+			icon: Banknote,
+			isActive: false,
+			isPage: true,
+		},
+		{
+			title: "Cadastro",
+			url: "#",
+			icon: Database,
+			items: [
+				{
+					title: "Clientes",
+					url: "#",
+				},
+				{
+					title: "Produtos",
+					url: "#",
+				},
+				{
+					title: "Colaboradores",
+					url: "#",
+				},
+			],
+		},
+		{
+			title: "Loja Online",
+			url: "#",
+			icon: MonitorSmartphone,
+			items: [
+				{
+					title: "Geral",
+					url: "#",
+				},
+				{
+					title: "Itens da Loja",
+					url: "#",
+				},
+				{
+					title: "Aparência",
+					url: "#",
+				},
+				{
+					title: "Formas de Pagamento",
+					url: "#",
+				},
+			],
+		},
+		{
+			title: "Marketing",
+			url: "#",
+			icon: Rocket,
+			items: [
+				{
+					title: "WhatsApp Marketing",
+					url: "#",
+				},
+				{
+					title: "Cupons",
+					url: "#",
+				},
+				{
+					title: "Fidelidade",
+					url: "#",
+				},
+			],
+		},
+		{
+			title: "Fiscal",
+			url: "#",
+			icon: Receipt,
+			isActive: false,
+			isPage: true,
+		},
+		{
+			title: "Certificado Digital",
+			url: "#",
+			icon: ShieldCheck,
+			isActive: false,
+			isPage: true,
+		},
+		{
+			title: "Configurações",
+			url: "#",
+			icon: Settings,
+			isActive: false,
+			isPage: true,
+		},
+	],
+};
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+	return (
+		<Sidebar collapsible="icon" {...props}>
+			<SidebarHeader>
+				<TeamSwitcher teams={data.teams} />
+			</SidebarHeader>
+
+			<SidebarContent>
+				<NavMain items={data.navMain} />
+			</SidebarContent>
+
+			<SidebarFooter>
+				<NavUser user={data.user} />
+			</SidebarFooter>
+
+			<SidebarRail />
+		</Sidebar>
+	);
+}
